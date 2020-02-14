d9e6cf1a1888168ca6ebba63e8747c020f9f3e38
sevilx <sevilxiong@gmail.com>
2020-02-14 16:09:13 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,5 +5,19 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# 定制默认IP,替换默认的192.168.1.1
+#sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+
+# Change timezone
+#cd ../..
+#sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
+
+# Change default theme
+#sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
+
+# Remove bootstrap theme
+#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+
+#CONFIG_KERNEL_BUILD_USER="Compiled by Ben"
+#CONFIG_GRUB_TITLE="OpenWrt AutoBuild"
