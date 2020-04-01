b3e309cbde712fed8727d58c3a93a23c807d639d
sevilx <sevilxiong@gmail.com>
2020-04-01 19:27:02 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -6,14 +6,11 @@
 # Blog: https://p3terx.com
 #=================================================
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
 #echo '修改机器名称'
 #sed -i 's/OpenWrt/OpenWrt-Ben/g' package/base-files/files/bin/config_generate
 
-#echo '修改网关地址'
-#sed -i 's/192.168.1.1/192.168.99.1/g' package/base-files/files/bin/config_generate
+#echo '修改默认地址'
+sed -i 's/192.168.1.1/192.168.99.253/g' package/base-files/files/bin/config_generate
 
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
