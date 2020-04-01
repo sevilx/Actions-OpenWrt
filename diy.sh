4e1bddbdb7993962c0104b8e1a6e76fdd9fc3378
sevilx <sevilxiong@gmail.com>
2020-04-01 08:56:57 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -29,20 +29,20 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon_new/g' package/feeds/luci/luci/M
 
 # 添加第三方软件包
 # 复杂的AdGuardHome的openwrt的luci界面
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/lean/luci-app-adguardhome
 # DiskMan for LuCI (WIP)
-# git clone https://github.com/lisaac/luci-app-diskman.git package/luci-app-diskman
-# mkdir -p package/mine/parted && cp -i package/mine/luci-app-diskman/Parted.Makefile package/parted/Makefile
+# git clone https://github.com/lisaac/luci-app-diskman.git package/lean/luci-app-diskman
+# mkdir -p package/mine/parted && cp -i package/mine/luci-app-diskman/Parted.Makefile package/lean/parted/Makefile
 # KPR plus+
-# git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/luci-app-koolproxyR
+# git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/lean/luci-app-koolproxyR
 # Server酱
-# git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+# git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-app-serverchan
 # FileBrowser
-# git clone https://github.com/project-openwrt/FileBrowser.git package/FileBrowser
+# git clone https://github.com/project-openwrt/FileBrowser.git package/lean/FileBrowser
 # OpenClash
-# git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+# git clone https://github.com/vernesong/OpenClash.git package/lean/OpenClash
 # 管控上网行为
-# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
 
 echo '修改banner'
 rm -rf package/base-files/files/etc/banner
