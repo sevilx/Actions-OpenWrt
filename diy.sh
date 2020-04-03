f8de7a856954464eca6e87c05a2e62a8748b93f8
sevilx <sevilxiong@gmail.com>
2020-04-03 21:37:48 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -15,11 +15,9 @@ sed -i 's/192.168.1.1/192.168.99.253/g' package/base-files/files/bin/config_gene
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
-echo '修改默认主题为Argon'
+echo '去除默认bootstrap主题+修改默认主题为Argon'
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
-
-#echo '去除默认bootstrap主题'
-#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 
 # 删除旧版argon,拉去新源，设置新版argon为默认主题
 rm -rf ./package/lean/luci-theme-argon
