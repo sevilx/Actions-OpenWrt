8d0c622bcc375525d30f529e8a6a29bac16c6b70
sevilx <sevilxiong@gmail.com>
2020-04-05 22:44:27 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -12,6 +12,9 @@
 #echo '修改默认地址'
 sed -i 's/192.168.1.1/192.168.99.253/g' package/base-files/files/bin/config_generate
 
+# 修改版本
+sed -i "s/OpenWrt /Ben build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
@@ -48,3 +51,13 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git -b 18.06 package/lea
 echo '修改banner'
 rm -rf package/base-files/files/etc/banner
 cp -f ../banner package/base-files/files/etc/
+
+# Add kernel build user
+[ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_USER="Ben"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"P3TERX"@' .config
+
+# Add kernel build domain
+[ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
