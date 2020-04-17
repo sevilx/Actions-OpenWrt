4b8cc4b1143dad04d20fcac08bb82f49bd375a05
sevilx <sevilxiong@gmail.com>
2020-04-17 18:51:49 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -18,6 +18,9 @@ sed -i "s/OpenWrt /Ben build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/
 echo '修改时区'
 sed -i "s/'UTC'/'CST-8'\n        set system.@system[-1].zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
+# 增加HelloWorld
+sed -i 's/\#src-git helloworld/\src-git helloworld/g' ./feeds.conf.default
+
 echo '去除默认bootstrap主题+修改默认主题为Argon'
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
 sed -i 's/config internal themes/config internal themes\n    option Argon  \"\/luci-static\/argon\"/g' feeds/luci/modules/luci-base/root/etc/config/luci
